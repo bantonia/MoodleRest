1c3f7db4cc0fe8d085dae141438c9758748524e1
bantonia <bantonia@users.noreply.github.com>
2013-11-05 09:32:47 +0000

    Update MoodleRestException.java

--- src/net/beaconhillcott/moodlerest/MoodleRestException.java
+++ src/net/beaconhillcott/moodlerest/MoodleRestException.java
@@ -18,11 +18,13 @@
 
 package net.beaconhillcott.moodlerest;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Bill Antonia
  */
-public class MoodleRestException extends Exception {
+public class MoodleRestException extends Exception implements Serializable {
 
     /**
      *
