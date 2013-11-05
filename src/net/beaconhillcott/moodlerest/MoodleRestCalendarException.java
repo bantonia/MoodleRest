1451ec78f8341c403c7c9cad8ee8514b1193b1e2
bantonia <bantonia@users.noreply.github.com>
2013-11-05 09:26:57 +0000

    Update MoodleRestCalendarException.java

--- src/net/beaconhillcott/moodlerest/MoodleRestCalendarException.java
+++ src/net/beaconhillcott/moodlerest/MoodleRestCalendarException.java
@@ -4,11 +4,13 @@
  */
 package net.beaconhillcott.moodlerest;
 
+import java.io.Serializable;
+
 /**
  *
  * @author root
  */
-public class MoodleRestCalendarException extends MoodleRestException {
+public class MoodleRestCalendarException extends MoodleRestException implements Serializable {
   
   public static final String NO_LEGACY_CALL="No legacy call";
     
