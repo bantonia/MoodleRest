8f3b9f396b080c02f8168f15a9226d4d6093999b
bantonia <bill@beaconhillcott.net>
2013-11-05 18:46:10 +0000

    Modified Role class enum

--- src/net/beaconhillcott/moodlerest/Role.java
+++ src/net/beaconhillcott/moodlerest/Role.java
@@ -14,14 +14,14 @@ import java.io.Serializable;
  */
 public enum Role implements Serializable {
   
-  ROLE_MANAGER(1),
-  ROLE_COURSE_CREATOR(2),
-  ROLE_TEACHER(3),
-  ROLE_NON_EDITING_TEACHER(4),
-  ROLE_STUDENT(5),
-  ROLE_GUEST(6),
-  ROLE_AUTHENTICATED_USER(7),
-  ROLE_AUTHENTICATED_USER_ON_FRONT_PAGE(8);
+  MANAGER(1),
+  COURSE_CREATOR(2),
+  TEACHER(3),
+  NON_EDITING_TEACHER(4),
+  STUDENT(5),
+  GUEST(6),
+  AUTHENTICATED_USER(7),
+  AUTHENTICATED_USER_ON_FRONT_PAGE(8);
   
   private final int value;
   
