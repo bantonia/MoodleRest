9aac0dd237810b836db80eac2b4a935d3d48ea9c
bantonia <bantonia@users.noreply.github.com>
2013-11-05 09:38:52 +0000

    Update MoodleRestGroupException.java

--- src/net/beaconhillcott/moodlerest/MoodleRestGroupException.java
+++ src/net/beaconhillcott/moodlerest/MoodleRestGroupException.java
@@ -18,11 +18,13 @@
 
 package net.beaconhillcott.moodlerest;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Bill Antonia
  */
-public class MoodleRestGroupException extends MoodleRestException {
+public class MoodleRestGroupException extends MoodleRestException implements Serializable {
 
     /**
      *
